8ce1161b5ef7547b79a0747105a2d3540445c16e
draall <draall@gmail.com>
2018-07-10 03:14:12 +0900

    First Commit

---
+++ weldfile.py
@@ -0,0 +1,57 @@
+import re
+from pandas import Series, DataFrame
+
+
+listLineID = []
+listPipingSpec = []
+listWeldNo = []
+listCat = []
+listSize = []
+listType = []
+listTempQtyChk = []
+
+
+
+with open('D:\\w1\\a.w2', 'r') as file:
+    lines = file.readlines()
+    line = ''
+
+    for line in lines:
+        line = line + '                                  '
+        if line.find('PIPELINE REF',0,15)>=0:
+            wLineID = line.split(':-')[1].strip()
+        if line.find('PIPING SPEC',0,13)>=0:
+            wPipingSpec = line.split(':-')[1].strip()
+
+        if bool(re.match('\d[0-9 ]', line[2:4])):
+            wNo = line[2:4].strip()
+            wCat = line[19:22].strip()
+            wSize = line[25:32].strip()
+            wType = line[32:43].strip()
+
+            listLineID.append(wLineID)
+            listPipingSpec.append(wPipingSpec)
+            listWeldNo.append(wNo)
+            listCat.append(wCat)
+            listSize.append(wSize)
+            listType.append(wType)
+            
+    
+    data = {'PipeID': listLineID,
+            'PipingSpec': listPipingSpec,
+            'WeldNo': listWeldNo,
+            'WeldCat': listCat,
+            'WeldSize': listSize,
+            'WeldType': listType}
+    wdd = DataFrame(data)
+    wdd
+            
+
+
+
+
+
+            
+        
+
+
